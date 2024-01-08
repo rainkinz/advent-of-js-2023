c4aac96b0512fd2452b6f7dea767fc0e12ded92f
Brendan Grainger <brendan.grainger@gmail.com>
2024-01-08 17:19:38 -0500

    wired up graphql

--- web/src/pages/NewEventPage/NewEventPage.tsx
+++ web/src/pages/NewEventPage/NewEventPage.tsx
@@ -1,18 +1,49 @@
 import { Form, TextField, Label, DateField } from '@redwoodjs/forms'
-import { MetaTags } from '@redwoodjs/web'
+import { MetaTags, useMutation } from '@redwoodjs/web'
 
 import Checkbox from 'src/components/Checkbox/Checkbox'
 import HeaderWithRulers from 'src/components/HeaderWithRulers/HeaderWithRulers'
 
+const CREATE_EVENT_MUTATION = gql`
+  mutation createEventMutation(
+    $name: String!
+    $date: DateTime!
+    $sendReminder: Boolean!
+  ) {
+    createEvent(
+      input: {
+        userId: 1
+        sendReminder: $sendReminder
+        date: $date
+        name: $name
+      }
+    ) {
+      id
+      name
+      date
+    }
+  }
+`
+
 const NewEventPage = () => {
+  const [createEvent, createEventStatus] = useMutation(CREATE_EVENT_MUTATION)
+
   const handleSubmit = (data) => {
     console.log({ data })
+    createEvent({
+      variables: {
+        name: data.eventName,
+        date: data.eventDate,
+        sendReminder: data.eventReminder,
+      },
+    })
   }
+
   return (
     <>
       <MetaTags title="Setup your event" />
 
-      <HeaderWithRulers className="text-white" heading="Setup your event" />
+      <HeaderWithRulers className="text-white" heading="Set up your event" />
       <div className="auth-form">
         <Form onSubmit={handleSubmit}>
           <div className="field">
