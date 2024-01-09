b4f6696e09ad641b140eca6e0f4fe0d228636af8
Brendan Grainger <brendan.grainger@gmail.com>
2024-01-09 11:27:58 -0500

    Added styling for invites page

--- web/src/pages/EventInvitesPage/EventInvitesPage.tsx
+++ web/src/pages/EventInvitesPage/EventInvitesPage.tsx
@@ -9,15 +9,20 @@ const EventInvitesPage = () => {
     <>
       <MetaTags title="Invite friends and family" />
 
-      <h3>3 Days Until</h3>
-      <h1>Smith Christmas</h1>
-
-      <button>
-        <Icon id="edit" />
-      </button>
-      <Button size="small" className="bg-supernova text-black">
-        Match
-      </Button>
+      <h3 className="font-handwriting text-4xl uppercase text-white">
+        3 Days Until
+      </h3>
+      <div className="flex items-center gap-3">
+        <h1 className="m-0 flex-1 p-0 font-condensed text-[166px] uppercase leading-[0.8] text-white">
+          Smith Christmas
+        </h1>
+        <button className="text-white dark:text-white">
+          <Icon id="edit" />
+        </button>
+        <Button size="small" className="bg-supernova text-black">
+          Match
+        </Button>
+      </div>
     </>
   )
 }
